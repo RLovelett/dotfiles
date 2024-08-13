ac011a0af7bec427d2e728e4cd2bca64bdc02438
Ryan Lovelett <ryan@lovelett.me>
2024-08-13 11:16:10 -0400

    Enable nvim-tree

---
+++ .config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'nvim-tree/nvim-tree.lua',
+    lazy = true,
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      require('nvim-tree').setup {}
+    end,
+  },
+}
