b3f2282d13e108ec6d7091c7c8749163d861ccbe
Ryan Lovelett <ryan@lovelett.me>
2025-06-17 17:43:58 -0400

    Update plugins and refactor which-key config for new API
    Ran plugin updates via lazy, which updated multiple entries in lazy-lock.json.
    The update to which-key.nvim required changes to its configuration. The setup
    was refactored to use the `keys` field with group declarations instead of
    runtime registration via `require('which-key').register`. Visual mode key
    descriptions were also migrated to the new format.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -276,24 +276,18 @@ require('lazy').setup({
   { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+    keys = {
+      { '<leader>c', group = '[C]ode' },
+      { '<leader>d', group = '[D]ocument' },
+      { '<leader>r', group = '[R]ename' },
+      { '<leader>s', group = '[S]earch' },
+      { '<leader>w', group = '[W]orkspace' },
+      { '<leader>t', group = '[T]oggle' },
+      { '<leader>h', group = 'Git [H]unk' },
 
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
-    end,
+      -- Visual mode
+      { '<leader>h', desc = 'Git [H]unk', mode = 'v' },
+    },
   },
 
   -- NOTE: Plugins can specify dependencies.
