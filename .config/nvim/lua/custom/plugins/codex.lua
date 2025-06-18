016d1001bbaa297e9c4e97375337b4b5cdd2448e
Ryan Lovelett <ryan@lovelett.me>
2025-06-18 16:25:07 -0400

    Add codex.nvim plugin with toggle keybinding
    Adds `johnseth97/codex.nvim` as a lazy-loaded plugin. Registers
    `<leader>cc` to toggle the Codex popup. Configures the plugin with
    rounded borders, 80% width and height, internal keymaps disabled, and
    autoinstall enabled.

---
+++ .config/nvim/lua/custom/plugins/codex.lua
@@ -0,0 +1,20 @@
+return {
+  'johnseth97/codex.nvim',
+  lazy = true,
+  keys = {
+    {
+      '<leader>cc',
+      function()
+        require('codex').toggle()
+      end,
+      desc = 'Toggle Codex popup',
+    },
+  },
+  opts = {
+    keymaps = {}, -- disable internal mapping
+    border = 'rounded', -- or 'double'
+    width = 0.8,
+    height = 0.8,
+    autoinstall = true,
+  },
+}
