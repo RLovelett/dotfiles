c92426f2ce6d2d616c2106df1e5e7217f04e3498
Ryan Lovelett <RYAN.A.LOVELETT@saic.com>
2025-06-26 14:44:20 -0400

    WIP: This works but still has a bunch of "debug" in it

--- .config/zsh/login.d/50-java.zsh
+++ .config/zsh/login.d/50-java.zsh
@@ -1,3 +1,4 @@
 # Java Home (only if java_home exists) and LESS charset
 [[ -x /usr/libexec/java_home ]] && export JAVA_HOME=$(/usr/libexec/java_home --version 1.8)
-export LESSCHARSET=utf-8
+
+echo $PATH
