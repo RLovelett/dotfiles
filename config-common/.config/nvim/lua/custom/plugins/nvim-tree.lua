0c15ea342ea92d4489a1fe00a984fcae76a8a1ee
Ryan Lovelett <ryan@lovelett.me>
2025-07-21 14:17:11 -0400

    Update NeoVim configuration with Kickstart changes
    This commit updates the NeoVim configuration to incorporate upstream
    Kickstart changes. Updates include migration from vim.opt to vim.o for
    options, improved clipboard handling with scheduled setting after
    UiEnter, enhanced which-key configuration with delay and icon settings,
    addition of guess-indent.nvim for tab detection, updated Telescope with
    streamlined keymaps, migration to blink.cmp for completion, and
    restructured LSP keymaps using 'gr' prefix. The lazy-lock.json file is
    updated to reflect new plugin versions, and help documentation files are
    added for local reference.

--- config-common/.config/nvim/lua/custom/plugins/nvim-tree.lua
+++ config-common/.config/nvim/lua/custom/plugins/nvim-tree.lua
@@ -1,9 +1,12 @@
 return {
   {
     'nvim-tree/nvim-tree.lua',
-    lazy = true,
+    lazy = false,
     dependencies = {
       'nvim-tree/nvim-web-devicons',
+    },
+    keys = {
+      { '<leader>e', '<cmd>NvimTreeToggle<cr>', desc = 'Toggle File Explorer' },
     },
     config = function()
       require('nvim-tree').setup {}
