fc25ebb0866796d0850174ab4da7883e71053a8b
Ryan Lovelett <RYAN.A.LOVELETT@SAIC.COM>
2025-07-22 11:01:13 -0400

    Install cask applications to $HOME

--- config-common/.config/zsh/init.d/10-homebrew.zsh
+++ config-common/.config/zsh/init.d/10-homebrew.zsh
@@ -3,5 +3,6 @@
 if [[ -x "$HOMEBREW_PREFIX/bin/brew" ]]; then
   export HOMEBREW_ANALYTICS_DEBUG=1
   export HOMEBREW_NO_ANALYTICS=1
+  export HOMEBREW_CASK_OPTS="--appdir=$HOME/Applications"
   eval "$($HOMEBREW_PREFIX/bin/brew shellenv)"
 fi
