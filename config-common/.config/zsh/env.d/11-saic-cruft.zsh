7be977866a6ba85588359d338755cf3771d1cbf0
Ryan Lovelett <RYAN.A.LOVELETT@saic.com>
2025-08-13 11:01:17 -0400

    Add SAIC specific Zsh environment setup and CA certificate
    This commit introduces a new Zsh environment setup file,
    `11-saic-cruft.zsh`. The file sets the `UV_NATIVE_TLS` environment
    variable and specifies the location of an extra CA certificate required
    for certain operations.
    
    Additionally, the commit adds the `saic-private-root-ca-2016-01.crt`
    certificate file to the expected directory. This certificate file is
    used to establish trust for secure communications.

---
+++ config-common/.config/zsh/env.d/11-saic-cruft.zsh
@@ -0,0 +1,5 @@
+# https://docs.astral.sh/uv/concepts/authentication/#http-authentication
+export UV_NATIVE_TLS=true
+
+export NODE_EXTRA_CA_CERTS=~/.local/share/ca-certificates/saic/saic-private-root-ca-2016-01.crt
+
