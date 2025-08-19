809d0cb89a932da8c0127cd65ae3d5e6ac41b4dc
Ryan Lovelett <RYAN.A.LOVELETT@saic.com>
2025-08-19 18:02:04 -0400

    Add dracula theme to fzf

--- config-common/.config/zsh/env.d/11-fzf.zsh
+++ config-common/.config/zsh/env.d/11-fzf.zsh
@@ -1,1 +1,8 @@
-export FZF_CTRL_R_OPTS="--border --padding 1,2 --border-label ' History ' --tac --layout=reverse-list --tmux center"
+# Color for Dracula
+# Set FZF_DEFAULT_OPTS if unset
+: ${FZF_DEFAULT_OPTS:="
+  --color=fg:#f8f8f2,bg:#282a36,hl:#bd93f9
+  --color=fg+:#f8f8f2,bg+:#44475a,hl+:#bd93f9
+  --color=info:#ffb86c,prompt:#50fa7b,pointer:#ff79c6
+  --color=marker:#ff79c6,spinner:#ffb86c,header:#6272a4
+"}
