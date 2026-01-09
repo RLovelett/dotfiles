a597433939266c02f61276b36bcc8f863530f274
Ryan Lovelett <ryan@lovelett.me>
2026-01-09 11:43:39 -0500

    Migrate my Neovim config; deprecating kickstart

--- config-common/.config/nvim/lua/config/lazy.lua
+++ config-common/.config/nvim/lua/config/lazy.lua
@@ -2,4 +2,46 @@ vim.pack.add {
   { src = 'https://github.com/folke/lazy.nvim.git' },
 }
 
-require("lazy").setup("plugins")
+-- [[ Configure and install plugins ]]
+--
+--  To check the current status of your plugins, run
+--    :Lazy
+--
+--  You can press `?` in this menu for help. Use `:q` to close the window
+--
+--  To update plugins you can run
+--    :Lazy update
+--
+-- NOTE: Here is where you install your plugins.
+require('lazy').setup({
+  -- Import plugins from lua/plugins/*.lua
+  { import = 'plugins' },
+  -- Import plugins from lua/custom/plugins/*.lua
+  { import = 'custom.plugins' },
+  -- Import kickstart plugins
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.gitsigns',
+}, {
+  ui = {
+    -- If you are using a Nerd Font: set icons to an empty table which will use the
+    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
+    icons = vim.g.have_nerd_font and {} or {
+      cmd = '⌘',
+      config = '🛠',
+      event = '📅',
+      ft = '📂',
+      init = '⚙',
+      keys = '🗝',
+      plugin = '🔌',
+      runtime = '💻',
+      require = '🌙',
+      source = '📄',
+      start = '🚀',
+      task = '📌',
+      lazy = '💤 ',
+    },
+  },
+})
