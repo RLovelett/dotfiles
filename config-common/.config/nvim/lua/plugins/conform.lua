3a15160743ee430599a5f4bbc1b9b0d2358722d2
Ryan Lovelett <ryan@lovelett.me>
2026-04-03 09:53:01 -0400

    Switch Python formatter to ruff_format
    Replaces the commented-out `isort`/`black` formatter chain with
    `ruff_format` in the `conform.lua` formatter config.

--- config-common/.config/nvim/lua/plugins/conform.lua
+++ config-common/.config/nvim/lua/plugins/conform.lua
@@ -31,9 +31,8 @@ return {
     end,
     formatters_by_ft = {
       lua = { 'stylua' },
+      python = { 'ruff_format' },
       -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
-      --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
     },
