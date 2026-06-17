39b61aa92659a4cf1b6cc8c0605178398acf165c
Ryan Lovelett <RYAN.A.LOVELETT@saic.com>
2026-06-17 10:20:14 -0400

    Try some colors on indent

--- config-common/.config/nvim/lua/kickstart/plugins/indent_line.lua
+++ config-common/.config/nvim/lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,31 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = function(_, opts)
+      opts = vim.tbl_deep_extend('force', opts or {}, {
+        indent = {
+          'RainbowRed',
+          'RainbowYellow',
+          'RainbowBlue',
+          'RainbowOrange',
+          'RainbowGreen',
+          'RainbowViolet',
+          'RainbowCyan',
+        },
+      })
+      local colors = require('dracula').colors()
+
+      local hooks = require 'ibl.hooks'
+      hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+        vim.api.nvim_set_hl(0, 'RainbowRed', { fg = colors.red })
+        vim.api.nvim_set_hl(0, 'RainbowYellow', { fg = colors.yellow })
+        vim.api.nvim_set_hl(0, 'RainbowBlue', { fg = '#61AFEF' })
+        vim.api.nvim_set_hl(0, 'RainbowOrange', { fg = colors.orange })
+        vim.api.nvim_set_hl(0, 'RainbowGreen', { fg = colors.green })
+        vim.api.nvim_set_hl(0, 'RainbowViolet', { fg = colors.purple })
+        vim.api.nvim_set_hl(0, 'RainbowCyan', { fg = colors.cyan })
+      end)
+      return opts
+    end,
   },
 }
