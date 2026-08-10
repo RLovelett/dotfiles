a3cf805244c28e7279b206bb8c4fe590d717822b
Ryan Lovelett <ryan@lovelett.me>
2026-08-10 17:43:27 -0400

    Align tmux styling with Signal Rail
    Replace external tmux themes with declarative Dracula color
    tokens and status formatting. Publish Zsh vi-mode state through
    pane options for native INSERT and NORMAL indicators.

--- config-common/.config/zsh/init.d/85-vi-mode.zsh
+++ config-common/.config/zsh/init.d/85-vi-mode.zsh
@@ -1,9 +1,13 @@
-# Vi mode cursor indicators
-export ZSH_TMUX_MODE_FILE="${XDG_RUNTIME_DIR:-/tmp}/zsh_vi_mode_$$"
+# Vi mode cursor and tmux status indicators
+function _set_tmux_vi_mode {
+  [[ -n ${TMUX_PANE:-} ]] || return
+  tmux set-option -p -t "$TMUX_PANE" @zsh_vi_mode "$1" 2>/dev/null
+  tmux refresh-client -S 2>/dev/null
+}
 
 function zle-line-init {
   echo -ne "\e[5 q" # beam cursor on startup (insert mode)
-  echo "INSERT" >| "$ZSH_TMUX_MODE_FILE"
+  _set_tmux_vi_mode INSERT
 }
 
 # Change cursor shape for different vi modes
@@ -11,24 +15,24 @@ function zle-keymap-select {
   case $KEYMAP in
     vicmd)
       echo -ne '\e[1 q' # block cursor for normal mode
-      echo "NORMAL" >| "$ZSH_TMUX_MODE_FILE"
+      _set_tmux_vi_mode NORMAL
       ;;
     viins|main)
       echo -ne '\e[5 q' # beam cursor for insert mode
-      echo "INSERT" >| "$ZSH_TMUX_MODE_FILE"
+      _set_tmux_vi_mode INSERT
       ;;
   esac
   zle reset-prompt
 }
 
 function zle-line-finish {
-  echo "INSERT" >| "$ZSH_TMUX_MODE_FILE"
+  _set_tmux_vi_mode INSERT
 }
 
 # Register the functions with ZLE
 zle -N zle-line-init
 zle -N zle-keymap-select
-zle -N zle-line-init
+zle -N zle-line-finish
 
 # Reduce ESC delay (make vi mode more responsive)
 export KEYTIMEOUT=1
